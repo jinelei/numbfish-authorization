0410328028ee906360decf31a31b31900f22526e
jinelei <jinelei@163.com>
2025-03-15 17:46:46 +0800

    移除无用导入

--- auth-api/src/main/java/com/jinelei/iotgenius/auth/dto/user/UserLoginRequest.java
+++ auth-api/src/main/java/com/jinelei/iotgenius/auth/dto/user/UserLoginRequest.java
@@ -2,10 +2,7 @@ package com.jinelei.iotgenius.auth.dto.user;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
 import java.io.Serializable;
-import java.util.List;
 import java.util.Objects;
 
 @Schema(description = "用户登陆请求对象")
