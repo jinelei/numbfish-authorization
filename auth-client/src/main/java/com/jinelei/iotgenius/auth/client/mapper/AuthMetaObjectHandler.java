4fdf375fcc68d31f4c2960430e922ca346171378
jinelei <jinelei@gmail.com>
2025-04-04 23:23:26 +0800

    修复使用当前用户

--- auth-client/src/main/java/com/jinelei/iotgenius/auth/client/mapper/AuthMetaObjectHandler.java
+++ auth-client/src/main/java/com/jinelei/iotgenius/auth/client/mapper/AuthMetaObjectHandler.java
@@ -1,28 +1,42 @@
 package com.jinelei.iotgenius.auth.client.mapper;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.jinelei.iotgenius.auth.dto.user.UserResponse;
+import com.jinelei.iotgenius.auth.helper.AuthorizationHelper;
 import org.apache.ibatis.reflection.MetaObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+@SuppressWarnings("unused")
 @Component
 public class AuthMetaObjectHandler implements MetaObjectHandler {
     private static final Logger log = LoggerFactory.getLogger(AuthMetaObjectHandler.class);
 
     @Override
     public void insertFill(MetaObject metaObject) {
-        log.info("开始插入填充...");
-        this.strictInsertFill(metaObject, "createdUserId", Long.class, 123456L);
+        try {
+            Optional.ofNullable(AuthorizationHelper.currentUser())
+                    .map(UserResponse::getId)
+                    .ifPresent(id -> this.strictInsertFill(metaObject, "createdUserId", Long.class, id));
+        } catch (Exception e) {
+            log.error("获取当前用户信息失败", e);
+        }
         this.strictInsertFill(metaObject, "createdTime", LocalDateTime.class, LocalDateTime.now());
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        log.info("开始更新填充...");
-        this.strictInsertFill(metaObject, "updatedUserId", Long.class, 123456L);
+        try {
+            Optional.ofNullable(AuthorizationHelper.currentUser())
+                    .map(UserResponse::getId)
+                    .ifPresent(id -> this.strictInsertFill(metaObject, "updatedUserId", Long.class, id));
+        } catch (Exception e) {
+            log.error("获取当前用户信息失败", e);
+        }
         this.strictUpdateFill(metaObject, "updatedTime", LocalDateTime.class, LocalDateTime.now());
     }
 
