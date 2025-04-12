c3ab200934c71b196cda2f384fec3de4473d51d3
jinelei <jinelei@163.com>
2025-04-12 15:51:45 +0800

    移除无用的权限检查&修改命名

--- auth-client/src/main/java/com/jinelei/iotgenius/auth/client/controller/ClientController.java
+++ auth-client/src/main/java/com/jinelei/iotgenius/auth/client/controller/ClientController.java
@@ -50,7 +50,6 @@ public class ClientController implements ClientApi {
     @Operation(summary = "创建客户端")
     @PostMapping("/create")
     public BaseView<Void> create(@RequestBody @Valid ClientCreateRequest request) {
-        AuthorizationHelper.hasPermissions(PermissionInstance.CLIENT_CREATE);
         clientService.create(request);
         return new BaseView<>("创建成功");
     }
@@ -60,7 +59,6 @@ public class ClientController implements ClientApi {
     @Operation(summary = "删除客户端")
     @PostMapping("/delete")
     public BaseView<Void> delete(@RequestBody @Valid ClientDeleteRequest request) {
-        AuthorizationHelper.hasPermissions(PermissionInstance.CLIENT_DELETE);
         clientService.delete(request);
         return new BaseView<>("删除成功");
     }
@@ -70,7 +68,6 @@ public class ClientController implements ClientApi {
     @Operation(summary = "更新客户端")
     @PostMapping("/update")
     public BaseView<Void> update(@RequestBody @Valid ClientUpdateRequest request) {
-        AuthorizationHelper.hasPermissions(PermissionInstance.CLIENT_UPDATE);
         clientService.update(request);
         return new BaseView<>("更新成功");
     }
@@ -80,7 +77,6 @@ public class ClientController implements ClientApi {
     @Operation(summary = "获取客户端")
     @PostMapping("/get")
     public BaseView<ClientResponse> get(@RequestBody @Valid ClientQueryRequest request) {
-        AuthorizationHelper.hasPermissions(PermissionInstance.CLIENT_DETAIL);
         ClientEntity entity = clientService.get(request);
         ClientResponse convert = clientService.convert(entity);
         return new BaseView<>(convert);
@@ -91,7 +87,6 @@ public class ClientController implements ClientApi {
     @Operation(summary = "获取客户端列表")
     @PostMapping("/list")
     public ListView<ClientResponse> list(@RequestBody @Valid ClientQueryRequest request) {
-        AuthorizationHelper.hasPermissions(PermissionInstance.CLIENT_SUMMARY);
         List<ClientEntity> entities = clientService.list(request);
         List<ClientResponse> convert = entities.parallelStream().map(entity -> clientService.convert(entity))
                 .collect(Collectors.toList());
@@ -103,7 +98,6 @@ public class ClientController implements ClientApi {
     @Operation(summary = "获取客户端分页")
     @PostMapping("/page")
     public PageView<ClientResponse> page(@RequestBody @Valid PageRequest<ClientQueryRequest> request) {
-        AuthorizationHelper.hasPermissions(PermissionInstance.CLIENT_SUMMARY);
         IPage<ClientEntity> page = clientService.page(PageHelper.toPage(new PageDTO<>(), request), request.getParams());
         List<ClientResponse> collect = page.getRecords().parallelStream().map(entity -> clientService.convert(entity))
                 .collect(Collectors.toList());
--- auth-client/src/main/java/com/jinelei/iotgenius/auth/client/service/impl/ClientServiceImpl.java
+++ auth-client/src/main/java/com/jinelei/iotgenius/auth/client/service/impl/ClientServiceImpl.java
@@ -25,7 +25,6 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jinelei.iotgenius.auth.client.configuration.authentication.ClientDetailService;
-import com.jinelei.iotgenius.auth.client.configuration.authentication.permission.instance.PermissionInstance;
 import com.jinelei.iotgenius.auth.client.convertor.ClientConvertor;
 import com.jinelei.iotgenius.auth.client.convertor.PermissionConvertor;
 import com.jinelei.iotgenius.auth.client.convertor.RoleConvertor;
@@ -52,13 +51,13 @@ public class ClientServiceImpl extends ServiceImpl<ClientMapper, ClientEntity>
     private static final Logger log = LoggerFactory.getLogger(ClientServiceImpl.class);
 
     @Autowired
-    protected ClientConvertor userConvertor;
+    protected ClientConvertor clientConvertor;
     @Autowired
     protected RoleConvertor roleConvertor;
     @Autowired
     protected PermissionConvertor permissionConvertor;
     @Autowired
-    protected ClientPermissionService userRoleService;
+    protected ClientPermissionService clientPermissionService;
     @Autowired
     protected PasswordEncoder passwordEncoder;
     @Autowired
@@ -70,7 +69,7 @@ public class ClientServiceImpl extends ServiceImpl<ClientMapper, ClientEntity>
     @Autowired
     protected StringRedisTemplate stringRedisTemplate;
     @Autowired
-    protected RedisTemplate<String, ClientResponse> userRedisTemplate;
+    protected RedisTemplate<String, ClientResponse> clientRedisTemplate;
     @Autowired
     protected ObjectMapper objectMapper;
     @Autowired
@@ -78,28 +77,27 @@ public class ClientServiceImpl extends ServiceImpl<ClientMapper, ClientEntity>
 
     @Override
     public void create(ClientCreateRequest request) {
-        AuthorizationHelper.hasPermissions(PermissionInstance.CLIENT_CREATE);
-        final ClientEntity entity = userConvertor.entityFromCreateRequest(request);
-        Optional.ofNullable(entity).orElseThrow(() -> new InvalidArgsException("角色信息不合法"));
+        final ClientEntity entity = clientConvertor.entityFromCreateRequest(request);
+        Optional.ofNullable(entity).orElseThrow(() -> new InvalidArgsException("客户端信息不合法"));
         Optional.of(entity).map(ClientEntity::getSecretKey).ifPresentOrElse(password -> {
             if (password.length() < 6) {
                 throw new InvalidArgsException("密码长度不能小于6位");
             }
-            entity.setSecretKey(passwordEncoder.encode(password));
-        }, () -> entity.setSecretKey(passwordEncoder.encode(UUID.randomUUID().toString().replace("-", ""))));
+            entity.setSecretKey(password);
+        }, () -> entity.setSecretKey(UUID.randomUUID().toString().replace("-", "")));
         int inserted = baseMapper.insert(entity);
         log.debug("创建客户端: {}", inserted);
         Optional.ofNullable(request.getPermissionIds())
                 .filter(i -> !CollectionUtils.isEmpty(i))
                 .ifPresent(list -> {
-                    final List<ClientPermissionEntity> userRoleEntities = list.parallelStream()
+                    final List<ClientPermissionEntity> clientRoleEntities = list.parallelStream()
                             .map(id -> {
                                 ClientPermissionEntity clientPermissionEntity = new ClientPermissionEntity();
                                 clientPermissionEntity.setClientId(entity.getId());
                                 clientPermissionEntity.setPermissionId(id);
                                 return clientPermissionEntity;
                             }).toList();
-                    List<BatchResult> insert = userRoleService.getBaseMapper().insert(userRoleEntities);
+                    List<BatchResult> insert = clientPermissionService.getBaseMapper().insert(clientRoleEntities);
                     log.debug("创建客户端角色关系: {}", insert.size());
                 });
         Assert.state(inserted == 1, "角色创建失败");
@@ -107,23 +105,22 @@ public class ClientServiceImpl extends ServiceImpl<ClientMapper, ClientEntity>
 
     @Override
     public void delete(ClientDeleteRequest request) {
-        AuthorizationHelper.hasPermissions(PermissionInstance.CLIENT_DELETE);
         if (Objects.nonNull(request.getId())) {
             int deleted = baseMapper.deleteById(request.getId());
             log.debug("删除客户端: {}", deleted);
-            Assert.state(deleted == 1, "角色删除失败");
+            Assert.state(deleted == 1, "客户端删除失败");
             LambdaUpdateWrapper<ClientPermissionEntity> wrapper = Wrappers.lambdaUpdate(ClientPermissionEntity.class)
                     .eq(ClientPermissionEntity::getClientId, request.getId());
-            int deletedClientPermission = userRoleService.getBaseMapper().delete(wrapper);
-            log.debug("删除客户端角色关系: {}", deletedClientPermission);
+            int deletedClientPermission = clientPermissionService.getBaseMapper().delete(wrapper);
+            log.debug("删除客户端权限关系: {}", deletedClientPermission);
         } else if (!CollectionUtils.isEmpty(request.getIds())) {
             int deleted = baseMapper.deleteByIds(request.getIds());
             log.debug("删除客户端: {}", deleted);
-            Assert.state(deleted == request.getIds().size(), "角色删除失败");
-            int deletedClientPermission = userRoleService.getBaseMapper().delete(
+            Assert.state(deleted == request.getIds().size(), "客户端删除失败");
+            int deletedClientPermission = clientPermissionService.getBaseMapper().delete(
                     Wrappers.lambdaUpdate(ClientPermissionEntity.class).in(ClientPermissionEntity::getClientId,
                             request.getIds()));
-            log.debug("删除客户端角色关系: {}", deletedClientPermission);
+            log.debug("删除客户端权限关系: {}", deletedClientPermission);
         } else {
             throw new InvalidArgsException("不支持的删除方式: " + request);
         }
@@ -131,7 +128,6 @@ public class ClientServiceImpl extends ServiceImpl<ClientMapper, ClientEntity>
 
     @Override
     public void update(@Validated ClientUpdateRequest request) {
-        AuthorizationHelper.hasPermissions(PermissionInstance.CLIENT_UPDATE);
         final String accessKey = Optional.ofNullable(request).map(ClientUpdateRequest::getAccessKey)
                 .orElseThrow(() -> new InvalidArgsException("客户端密钥不能为空"));
         LambdaUpdateWrapper<ClientEntity> wrapper = Wrappers.lambdaUpdate(ClientEntity.class);
@@ -142,29 +138,28 @@ public class ClientServiceImpl extends ServiceImpl<ClientMapper, ClientEntity>
         wrapper.set(ClientEntity::getRemark, request.getRemark());
         int updated = baseMapper.update(wrapper);
         log.debug("更新客户端: {}", updated);
-        int deleted = userRoleService.getBaseMapper()
+        int deleted = clientPermissionService.getBaseMapper()
                 .delete(Wrappers.lambdaUpdate(ClientPermissionEntity.class).eq(ClientPermissionEntity::getClientId,
                         request.getId()));
-        log.debug("删除客户端角色关系: {}", deleted);
+        log.debug("删除客户端权限关系: {}", deleted);
         Optional.ofNullable(request.getPermissionIds())
                 .filter(i -> !CollectionUtils.isEmpty(i))
                 .ifPresent(list -> {
-                    final List<ClientPermissionEntity> userRoleEntities = list.parallelStream()
+                    final List<ClientPermissionEntity> clientRoleEntities = list.parallelStream()
                             .map(id -> {
                                 ClientPermissionEntity clientPermissionEntity = new ClientPermissionEntity();
                                 clientPermissionEntity.setClientId(request.getId());
                                 clientPermissionEntity.setPermissionId(id);
                                 return clientPermissionEntity;
                             }).toList();
-                    List<BatchResult> insert = userRoleService.getBaseMapper().insert(userRoleEntities);
-                    log.debug("创建客户端角色关系: {}", insert.size());
+                    List<BatchResult> insert = clientPermissionService.getBaseMapper().insert(clientRoleEntities);
+                    log.debug("创建客户端权限关系: {}", insert.size());
                 });
-        Assert.state(updated == 1, "角色更新失败");
+        Assert.state(updated == 1, "客户端更新失败");
     }
 
     @Override
     public ClientEntity get(ClientQueryRequest request) {
-        AuthorizationHelper.hasPermissions(PermissionInstance.CLIENT_DETAIL);
         LambdaQueryWrapper<ClientEntity> wrapper = Wrappers.lambdaQuery(ClientEntity.class);
         wrapper.eq(Objects.nonNull(request.getId()), ClientEntity::getId, request.getId());
         wrapper.like(Objects.nonNull(request.getAccessKey()), ClientEntity::getAccessKey, request.getAccessKey());
@@ -176,7 +171,6 @@ public class ClientServiceImpl extends ServiceImpl<ClientMapper, ClientEntity>
 
     @Override
     public List<ClientEntity> list(ClientQueryRequest request) {
-        AuthorizationHelper.hasPermissions(PermissionInstance.CLIENT_SUMMARY);
         LambdaQueryWrapper<ClientEntity> wrapper = Wrappers.lambdaQuery(ClientEntity.class);
         wrapper.eq(Objects.nonNull(request.getId()), ClientEntity::getId, request.getId());
         wrapper.like(Objects.nonNull(request.getAccessKey()), ClientEntity::getAccessKey, request.getAccessKey());
@@ -188,7 +182,6 @@ public class ClientServiceImpl extends ServiceImpl<ClientMapper, ClientEntity>
 
     @Override
     public IPage<ClientEntity> page(IPage<ClientEntity> page, ClientQueryRequest request) {
-        AuthorizationHelper.hasPermissions(PermissionInstance.CLIENT_SUMMARY);
         LambdaQueryWrapper<ClientEntity> wrapper = Wrappers.lambdaQuery(ClientEntity.class);
         wrapper.eq(Objects.nonNull(request.getId()), ClientEntity::getId, request.getId());
         wrapper.like(Objects.nonNull(request.getAccessKey()), ClientEntity::getAccessKey, request.getAccessKey());
@@ -200,15 +193,15 @@ public class ClientServiceImpl extends ServiceImpl<ClientMapper, ClientEntity>
 
     @Override
     public ClientResponse convert(ClientEntity entity) {
-        return userConvertor.entityToResponse(entity);
+        return clientConvertor.entityToResponse(entity);
     }
 
     @Override
     public ClientResponse loadClientByAccessKey(String accessKey) {
         Optional.ofNullable(accessKey).orElseThrow(() -> new UsernameNotFoundException("AccessKey不能为空"));
-        ClientQueryRequest clientQueryRequest = new ClientQueryRequest();
-        clientQueryRequest.setAccessKey(accessKey);
-        ClientEntity clientEntity = this.get(clientQueryRequest);
+        LambdaQueryWrapper<ClientEntity> wrapper = Wrappers.lambdaQuery(ClientEntity.class);
+        wrapper.eq(ClientEntity::getAccessKey, accessKey);
+        ClientEntity clientEntity = baseMapper.selectOne(wrapper);
         Optional.ofNullable(clientEntity).orElseThrow(() -> new UsernameNotFoundException("AccessKey不存在"));
         return convert(clientEntity);
     }
