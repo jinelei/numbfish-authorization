085fd1e531f8a74abaefc174d7db91aff521dfc8
jinelei <jinelei@163.com>
2025-04-12 20:11:50 +0800

    添加PreAuthorize支持

--- auth-api/src/main/java/com/jinelei/numbfish/auth/configuration/SecurityAutoConfiguration.java
+++ auth-api/src/main/java/com/jinelei/numbfish/auth/configuration/SecurityAutoConfiguration.java
@@ -5,9 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SuppressWarnings("unused")
 @Configuration
+@EnableMethodSecurity
 @Import({CoreSecurityConfiguration.class, AuthorizationProperty.class})
 public class SecurityAutoConfiguration {
     private static final Logger log = LoggerFactory.getLogger(SecurityAutoConfiguration.class);
