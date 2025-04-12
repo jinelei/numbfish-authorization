f9f979182194c543b889c641611cf9f836f25b25
jinelei <jinelei@163.com>
2025-04-12 20:11:30 +0800

    调整编码

--- auth-client/src/main/java/com/jinelei/numbfish/auth/client/configuration/authentication/instance/PermissionInstance.java
+++ auth-client/src/main/java/com/jinelei/numbfish/auth/client/configuration/authentication/instance/PermissionInstance.java
@@ -6,41 +6,41 @@ import com.jinelei.numbfish.auth.permission.declaration.PermissionDeclaration;
 @SuppressWarnings("unused")
 public enum PermissionInstance implements PermissionDeclaration<PermissionInstance> {
     /**
-     * 客户端相关
+     * 权限相关
      */
-    CLIENT_MANAGE("客户端管理", "user_manage", null, "客户端管理", PermissionType.DIRECTORY, 100),
-    CLIENT_CREATE("创建客户端", "user_create", CLIENT_MANAGE, "创建客户端", PermissionType.ACTION, 101),
-    CLIENT_UPDATE("更新客户端", "user_update", CLIENT_MANAGE, "更新客户端", PermissionType.ACTION, 102),
-    CLIENT_DELETE("删除客户端", "user_delete", CLIENT_MANAGE, "删除客户端", PermissionType.ACTION, 103),
-    CLIENT_SUMMARY("查看客户端概要", "user_summary", CLIENT_MANAGE, "查看客户端概要", PermissionType.ACTION, 104),
-    CLIENT_DETAIL("查看客户端详情", "user_detail", CLIENT_MANAGE, "查看客户端详情", PermissionType.ACTION, 105),
-    /**
-     * 用户相关
-     */
-    USER_MANAGE("用户管理", "user_manage", null, "用户管理", PermissionType.DIRECTORY, 100),
-    USER_CREATE("创建用户", "user_create", USER_MANAGE, "创建用户", PermissionType.ACTION, 101),
-    USER_UPDATE("更新用户", "user_update", USER_MANAGE, "更新用户", PermissionType.ACTION, 102),
-    USER_DELETE("删除用户", "user_delete", USER_MANAGE, "删除用户", PermissionType.ACTION, 103),
-    USER_SUMMARY("查看用户概要", "user_summary", USER_MANAGE, "查看用户概要", PermissionType.ACTION, 104),
-    USER_DETAIL("查看用户详情", "user_detail", USER_MANAGE, "查看用户详情", PermissionType.ACTION, 105),
+    PERMISSION_MANAGE("权限管理", "PERMISSION_MANAGE", null, "权限管理", PermissionType.DIRECTORY, 100),
+    PERMISSION_CREATE("创建权限", "PERMISSION_CREATE", PERMISSION_MANAGE, "创建权限", PermissionType.ACTION, 101),
+    PERMISSION_UPDATE("更新权限", "PERMISSION_UPDATE", PERMISSION_MANAGE, "更新权限", PermissionType.ACTION, 102),
+    PERMISSION_DELETE("删除权限", "PERMISSION_DELETE", PERMISSION_MANAGE, "删除权限", PermissionType.ACTION, 103),
+    PERMISSION_SUMMARY("查看权限概要", "PERMISSION_SUMMARY", PERMISSION_MANAGE, "查看权限概要", PermissionType.ACTION, 104),
+    PERMISSION_DETAIL("查看权限详情", "PERMISSION_DETAIL", PERMISSION_MANAGE, "查看权限详情", PermissionType.ACTION, 105),
     /**
      * 角色相关
      */
-    ROLE_MANAGE("角色管理", "role_manage", null, "角色管理", PermissionType.DIRECTORY, 200),
-    ROLE_CREATE("创建角色", "role_create", ROLE_MANAGE, "创建角色", PermissionType.ACTION, 201),
-    ROLE_UPDATE("更新角色", "role_update", ROLE_MANAGE, "更新角色", PermissionType.ACTION, 202),
-    ROLE_DELETE("删除角色", "role_delete", ROLE_MANAGE, "删除角色", PermissionType.ACTION, 203),
-    ROLE_SUMMARY("查看角色概要", "role_summary", ROLE_MANAGE, "查看角色概要", PermissionType.ACTION, 204),
-    ROLE_DETAIL("查看角色详情", "role_detail", ROLE_MANAGE, "查看角色详情", PermissionType.ACTION, 205),
+    ROLE_MANAGE("角色管理", "ROLE_MANAGE", null, "角色管理", PermissionType.DIRECTORY, 200),
+    ROLE_CREATE("创建角色", "ROLE_CREATE", ROLE_MANAGE, "创建角色", PermissionType.ACTION, 201),
+    ROLE_UPDATE("更新角色", "ROLE_UPDATE", ROLE_MANAGE, "更新角色", PermissionType.ACTION, 202),
+    ROLE_DELETE("删除角色", "ROLE_DELETE", ROLE_MANAGE, "删除角色", PermissionType.ACTION, 203),
+    ROLE_SUMMARY("查看角色概要", "ROLE_SUMMARY", ROLE_MANAGE, "查看角色概要", PermissionType.ACTION, 204),
+    ROLE_DETAIL("查看角色详情", "ROLE_DETAIL", ROLE_MANAGE, "查看角色详情", PermissionType.ACTION, 205),
     /**
-     * 权限相关
+     * 用户相关
      */
-    PERMISSION_MANAGE("权限管理", "permission_manage", null, "权限管理", PermissionType.DIRECTORY, 300),
-    PERMISSION_CREATE("创建权限", "permission_create", PERMISSION_MANAGE, "创建权限", PermissionType.ACTION, 301),
-    PERMISSION_UPDATE("更新权限", "permission_update", PERMISSION_MANAGE, "更新权限", PermissionType.ACTION, 302),
-    PERMISSION_DELETE("删除权限", "permission_delete", PERMISSION_MANAGE, "删除权限", PermissionType.ACTION, 303),
-    PERMISSION_SUMMARY("查看权限概要", "permission_summary", PERMISSION_MANAGE, "查看权限概要", PermissionType.ACTION, 304),
-    PERMISSION_DETAIL("查看权限详情", "permission_detail", PERMISSION_MANAGE, "查看权限详情", PermissionType.ACTION, 305),
+    USER_MANAGE("用户管理", "USER_MANAGE", null, "用户管理", PermissionType.DIRECTORY, 300),
+    USER_CREATE("创建用户", "USER_CREATE", USER_MANAGE, "创建用户", PermissionType.ACTION, 301),
+    USER_UPDATE("更新用户", "USER_UPDATE", USER_MANAGE, "更新用户", PermissionType.ACTION, 302),
+    USER_DELETE("删除用户", "USER_DELETE", USER_MANAGE, "删除用户", PermissionType.ACTION, 303),
+    USER_SUMMARY("查看用户概要", "USER_SUMMARY", USER_MANAGE, "查看用户概要", PermissionType.ACTION, 304),
+    USER_DETAIL("查看用户详情", "USER_DETAIL", USER_MANAGE, "查看用户详情", PermissionType.ACTION, 305),
+    /**
+     * 客户端相关
+     */
+    CLIENT_MANAGE("客户端管理", "CLIENT_MANAGE", null, "客户端管理", PermissionType.DIRECTORY, 400),
+    CLIENT_CREATE("创建客户端", "CLIENT_CREATE", CLIENT_MANAGE, "创建客户端", PermissionType.ACTION, 401),
+    CLIENT_UPDATE("更新客户端", "CLIENT_UPDATE", CLIENT_MANAGE, "更新客户端", PermissionType.ACTION, 402),
+    CLIENT_DELETE("删除客户端", "CLIENT_DELETE", CLIENT_MANAGE, "删除客户端", PermissionType.ACTION, 403),
+    CLIENT_SUMMARY("查看客户端概要", "CLIENT_SUMMARY", CLIENT_MANAGE, "查看客户端概要", PermissionType.ACTION, 404),
+    CLIENT_DETAIL("查看客户端详情", "CLIENT_DETAIL", CLIENT_MANAGE, "查看客户端详情", PermissionType.ACTION, 405),
     ;
 
     private final String name;
--- auth-client/src/main/java/com/jinelei/numbfish/auth/client/configuration/authentication/instance/RoleInstance.java
+++ auth-client/src/main/java/com/jinelei/numbfish/auth/client/configuration/authentication/instance/RoleInstance.java
@@ -10,31 +10,37 @@ import com.jinelei.numbfish.auth.permission.declaration.RoleDeclaration;
 public enum RoleInstance implements RoleDeclaration<RoleInstance> {
     SUPER_ADMINISTRATOR("超级管理员", "super_administrator", null, "超级管理员", RoleType.ADMIN, 10000, List.of()),
     /**
-     * 用户相关
-     */
-    USER_ADMIN("用户管理员", "user_admin", SUPER_ADMINISTRATOR, "用户管理员", RoleType.ADMIN, 10100,
-            List.of(PermissionInstance.USER_MANAGE, PermissionInstance.USER_CREATE, PermissionInstance.USER_UPDATE,
-                    PermissionInstance.USER_DELETE, PermissionInstance.USER_SUMMARY, PermissionInstance.USER_DETAIL)),
-    USER_ALLOCATOR("角色分配员", "user_allocator", USER_ADMIN, "角色分配员", RoleType.NORMAL, 10101,
-            List.of(PermissionInstance.USER_MANAGE, PermissionInstance.USER_UPDATE,
-                    PermissionInstance.USER_SUMMARY, PermissionInstance.USER_DETAIL)),
-    USER_NORMAL("普通用户", "user_normal", USER_ADMIN, "普通用户", RoleType.NORMAL, 10102,
-            List.of(PermissionInstance.USER_DETAIL, PermissionInstance.ROLE_DETAIL,
-                    PermissionInstance.PERMISSION_DETAIL)),
-    /**
-     * 角色相关
-     */
-    ROLE_ADMIN("角色管理员", "role_admin", SUPER_ADMINISTRATOR, "角色管理员", RoleType.ADMIN, 10200,
-            List.of(PermissionInstance.ROLE_MANAGE, PermissionInstance.ROLE_CREATE, PermissionInstance.ROLE_UPDATE,
-                    PermissionInstance.ROLE_DELETE, PermissionInstance.ROLE_SUMMARY, PermissionInstance.ROLE_DETAIL)),
-    /**
      * 权限相关
      */
-    PERMISSION_ADMIN("权限管理员", "permission_admin", SUPER_ADMINISTRATOR, "权限管理员", RoleType.ADMIN, 10300,
+    PERMISSION_ADMIN("权限管理员", "permission_admin", SUPER_ADMINISTRATOR, "权限管理员", RoleType.ADMIN, 10100,
             List.of(PermissionInstance.PERMISSION_MANAGE, PermissionInstance.PERMISSION_CREATE,
                     PermissionInstance.PERMISSION_UPDATE,
                     PermissionInstance.PERMISSION_DELETE, PermissionInstance.PERMISSION_SUMMARY,
                     PermissionInstance.PERMISSION_DETAIL)),
+    /**
+     * 角色相关
+     */
+    ROLE_ADMIN("角色管理员", "ROLE_ADMIN", SUPER_ADMINISTRATOR, "角色管理员", RoleType.ADMIN, 10200,
+            List.of(PermissionInstance.ROLE_MANAGE, PermissionInstance.ROLE_CREATE, PermissionInstance.ROLE_UPDATE,
+                    PermissionInstance.ROLE_DELETE, PermissionInstance.ROLE_SUMMARY, PermissionInstance.ROLE_DETAIL)),
+    /**
+     * 用户相关
+     */
+    USER_ADMIN("用户管理员", "USER_ADMIN", SUPER_ADMINISTRATOR, "用户管理员", RoleType.ADMIN, 10300,
+            List.of(PermissionInstance.USER_MANAGE, PermissionInstance.USER_CREATE, PermissionInstance.USER_UPDATE,
+                    PermissionInstance.USER_DELETE, PermissionInstance.USER_SUMMARY, PermissionInstance.USER_DETAIL)),
+    USER_ALLOCATOR("角色分配员", "USER_ALLOCATOR", USER_ADMIN, "角色分配员", RoleType.NORMAL, 10301,
+            List.of(PermissionInstance.USER_MANAGE, PermissionInstance.USER_UPDATE,
+                    PermissionInstance.USER_SUMMARY, PermissionInstance.USER_DETAIL)),
+    USER_NORMAL("普通用户", "USER_NORMAL", USER_ADMIN, "普通用户", RoleType.NORMAL, 10302,
+            List.of(PermissionInstance.USER_DETAIL, PermissionInstance.ROLE_DETAIL,
+                    PermissionInstance.PERMISSION_DETAIL)),
+    /**
+     * 客户端相关
+     */
+    CLIENT_ADMIN("客户端管理员", "CLIENT_ADMIN", SUPER_ADMINISTRATOR, "客户端管理员", RoleType.ADMIN, 10400,
+            List.of(PermissionInstance.CLIENT_MANAGE, PermissionInstance.CLIENT_CREATE, PermissionInstance.CLIENT_UPDATE,
+                    PermissionInstance.CLIENT_DELETE, PermissionInstance.CLIENT_SUMMARY, PermissionInstance.CLIENT_DETAIL)),
     ;
 
     private final String name;
